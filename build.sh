23188730e18975140188ad81270651261ef1a82d
erdii <me@erdii.engineering>
2024-09-29 19:43:57 +0200

    enable bazzite copr and install unl0kr

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree install golang
+
+curl -Lo /etc/yum.repos.d/_copr_erdii-bazzite-playground.repo https://copr.fedorainfracloud.org/coprs/erdii/bazzite-playground/repo/fedora-"${RELEASE}"/erdii-bazzite-playground-fedora-"${RELEASE}".repo
+
+rpm-ostree install unl0kr

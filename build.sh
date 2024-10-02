c470cb84dbd6014efab5eecc7adcfd6169dcd045
erdii <me@erdii.engineering>
2024-10-02 13:10:20 +0200

    f
    Signed-off-by: erdii <me@erdii.engineering>

--- build.sh
+++ build.sh
@@ -7,19 +7,18 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree install golang
 
 rpm-ostree install git \
-    && systemd-rpm-macros \
-    && gcc \
-    && wget \
-    && meson \
-    && ninja-build \
-    && inih-devel \
-    && libinput-devel \
-    && libgudev \
-    && libxkbcommon-devel \
-    && libdrm-devel \
-    && scdoc
+    systemd-rpm-macros \
+    gcc \
+    wget \
+    meson \
+    ninja-build \
+    inih-devel \
+    libinput-devel \
+    libgudev \
+    libxkbcommon-devel \
+    libdrm-devel \
+    scdoc
 
+# curl -Lo /etc/yum.repos.d/_copr_erdii-bazzite-playground.repo https://copr.fedorainfracloud.org/coprs/erdii/bazzite-playground/repo/fedora-"${RELEASE}"/erdii-bazzite-playground-fedora-"${RELEASE}".repo
 
-curl -Lo /etc/yum.repos.d/_copr_erdii-bazzite-playground.repo https://copr.fedorainfracloud.org/coprs/erdii/bazzite-playground/repo/fedora-"${RELEASE}"/erdii-bazzite-playground-fedora-"${RELEASE}".repo
-
-rpm-ostree install unl0kr
+# rpm-ostree install unl0kr

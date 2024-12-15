f2ac16611a2b562d692e3eaf7364cad0b50c123f
erdii <me@erdii.engineering>
2024-12-15 22:58:21 +0100

    use unl0kr-0.0.git.17.fafd46ec-1.fc41.x86_64 and unl0kr-dracut-0.0.git.17.fafd46ec-1.fc41.x86_64
    Signed-off-by: erdii <me@erdii.engineering>

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ curl -Lo /etc/yum.repos.d/_copr_erdii-bazzite-playground.repo \
     https://copr.fedorainfracloud.org/coprs/erdii/bazzite-playground/repo/fedora-"${RELEASE}"/erdii-bazzite-playground-fedora-"${RELEASE}".repo
 
 rpm-ostree install \
-    unl0kr-2.0.3-1.fc40.x86_64 \
-    unl0kr-dracut-2.0.3-1.fc40.x86_64 \
+    unl0kr-0.0.git.17.fafd46ec-1.fc41.x86_64 \
+    unl0kr-dracut-0.0.git.17.fafd46ec-1.fc41.x86_64 \
     golang \
     fedora-packager \
     fedora-review \
